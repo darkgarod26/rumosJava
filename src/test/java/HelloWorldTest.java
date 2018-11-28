560a4e6ebe38ff79b3b7fe2e3d46187cabefcaf8
darkgarod26 <rubensilvalopes86@gmail.com>
2018-11-28 20:20:25 +0000

    Add_commit_ignore

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void helloWorld(){
+
+    String hello = "Hello World";
+    Assert.assertTrue("Hello World".equals(hello));
+}
+
+}
